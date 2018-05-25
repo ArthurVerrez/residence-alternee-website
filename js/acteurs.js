b4394c1f67d5ccb52c6ae959c2363f106d56b220
ArthurVerrez <33450143+ArthurVerrez@users.noreply.github.com>
2018-05-25 15:19:27 +0200

    circle

--- js/acteurs.js
+++ js/acteurs.js
@@ -1,5 +1,13 @@
-var c=document.getElementById("canvas-acteurs");
-var ctx=c.getContext("2d");
+var canvas = document.getElementById('canvas-acteurs');
+if (canvas.getContext)
+{
+var ctx = canvas.getContext('2d'); 
+var X = canvas.width / 2;
+var Y = canvas.height / 2;
+var R = 150;
 ctx.beginPath();
-ctx.arc(100,100,50,0,2*Math.PI);
+ctx.arc(X, Y, R, 0, 0.9*Math.PI, false);
+ctx.lineWidth = 15;
+ctx.strokeStyle = '#000000';
 ctx.stroke();
+}
