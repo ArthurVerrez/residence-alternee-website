8edc92c676c83ed465a42f204be30c9bde9e5a15
ArthurVerrez <33450143+ArthurVerrez@users.noreply.github.com>
2018-05-25 17:08:48 +0200

    circle

--- js/acteurs.js
+++ js/acteurs.js
@@ -76,13 +76,56 @@ function circle_arcs_points(n,ctx,X,Y,R){
  }
 }
 
-var cat_acteurs=[["cat1","blue"], ["cat2","red"], ["cat3","magenta"], ["cat4","yellow"], ["cat5","green"], ["cat6","grey"]];
+//Il faut absolument que les acteurs d'une même catégorie se suivent
+var cat_acteurs=[[[0,1,2],"cat1","blue"], [[3,4],"cat2","red"],
+ [[5],"cat3","magenta"], [[6],"cat4","yellow"], [[7,8],"cat5","green"], [[9],"cat6","grey"]];
 
 //tab_acteurs[[nom,categorie d'acteur]]
 
-var tab_acteurs=[["acteur 1", 0],["acteur 2", 0],["acteur 3", 1],["acteur 4", 2],["acteur 5", 2],
-                    ["acteur 6", 2],["acteur 7", 3],["acteur 8", 4],["acteur 9", 5],["acteur 10", 6]];
+var tab_acteurs=["acteur 1","acteur 2","acteur 3","acteur 4","acteur 5","acteur 6","acteur 7","acteur 8","acteur 9","acteur 10"];
 
+var connexions=[[1,2],[6,2],[6,3]];
+
+
+function trace_acteurs(cat_acteurs,tab_acteurs,connexions){
+    var canvas = document.getElementById('canvas-acteurs');
+    if (canvas.getContext){
+        var ctx = canvas.getContext('2d'); 
+        var X = canvas.width / 2;
+        var Y = canvas.height / 2;
+        var R = 180;
+        ctx.lineWidth = 4;
+    }
+
+
+    n=tab_acteurs.length
+
+    //On affiche d'abord les arcs de cercle
+    for(i=0; i<cat_acteurs.length; i++){
+        for(j=0; j<cat_acteurs[0].length;j++){
+            ctx.beginPath();
+            ctx.arc(X, Y, R, j*(2*Math.PI/n), (j+1)*(2*Math.PI/n), false);
+            ctx.strokeStyle = cat_acteurs[2];
+            ctx.stroke();
+        }
+        
+    }
+
+
+    //Puis on affiche les points
+    R_points=3;
+    //ctx.fill();
+    R=R*(3/4);
+    for(i=0; i<n; i++){
+        ctx.beginPath();
+        ctx.arc(X+Math.cos(i*2*Math.PI/n)*R, Y+Math.sin(i*2*Math.PI/n)*R, R_points, 0, 2*Math.PI, false);
+        
+        ctx.stroke();
+    }
+
+
+
+}
 
 
 var tab = initialize_canvas();
