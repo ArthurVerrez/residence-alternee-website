420f25cf709aa3a2ed83687a150c873dea40f647
PaulGarnierMuller <paulo19@live.fr>
2018-05-26 00:07:28 +0200

    try text acteurs

--- js/acteurs.js
+++ js/acteurs.js
@@ -4,14 +4,14 @@ canvas.setAttribute('width',window.innerHeight+"px");
 var X = canvas.width / 2;
 var Y = canvas.height / 2;
 var R = X*(3/4);
-R_points=4;
+var R_points=4;
 
-R_pcircle=(3/4)*R
+var R_pcircle=(3/4)*R
 var width_arcs=15;
 var width_points=10;
 var width_connections=5;
 
-var ctx = canvas.getContext('2d'); 
+var ctx = canvas.getContext('2d');
 
 
 //Il faut absolument que les acteurs d'une même catégorie se suivent
@@ -54,8 +54,11 @@ function trace_acteurs(cat_acteurs,tab_acteurs,connexions){
   n=tab_acteurs.length
 
     //On affiche d'abord les arcs de cercle et les points
-    
+
     for(i=0; i<cat_acteurs.length; i++){
+
+//les noms des catégories
+
       for(j=cat_acteurs[i][0][0]; j<cat_acteurs[i][0][cat_acteurs[i][0].length-1]+1;j++){
         ctx.beginPath();
         ctx.arc(X, Y, R, j*(2*Math.PI/n)-Math.PI/n, (j+1)*(2*Math.PI/n)-Math.PI/n, false);
@@ -71,19 +74,44 @@ function trace_acteurs(cat_acteurs,tab_acteurs,connexions){
         ctx.lineWidth = width_points;
         ctx.stroke();
       }
+      //Nom des catégories
+
+      if (cat_acteurs[i][0].length == 1) then {
+
+        var angle = cat_acteurs[i][0][0]*(2*Math.PI/n);
+        var X1 = X + (4*R/3)*Math.cos(angle);
+        var Y1 = Y + (4*R/3)*Math.sin(angle);
+
+        ctx.beginPath();
+        ctx.font = "30px Georgia";
+        ctx.fillStyle = cat_acteurs[i][2];
+        ctx.fillText(cat_acteurs[i][1],X1,Y1);
+        ctx.stroke();
+
+
+      } else {
+
+        var taille = cat_acteurs[i][0].length -1;
+        var angle1 = cat_acteurs[i][0][0]*(2*Math.PI/n);
+        var angle2 = cat_acteurs[i][0][taille]*(2*Math.PI/n);
+        var bissectrice1 = (angle2 - angle1)/2 + angle1;
+        var X1 = X + (4*R/3)*Math.cos(bissectrice1);
+        var Y1 = Y + (4*R/3)*Math.sin(bissectrice1);
+
+        ctx.beginPath();
+        ctx.font = "30px Georgia";
+        ctx.fillStyle = cat_acteurs[i][2];
+        ctx.fillText(cat_acteurs[i][1],X1,Y1);
+        ctx.stroke();
+
     }
-
-    //Puis on affiche les connexions
-
-    //Puis on affiche les noms
-
-    //text_cercle(ctx,X,Y,R,cat_acteurs);
+    }
 
     ctx.strokeStyle="black";
     for(i=0; i<connexions.length;i++){
       circle_arcs_2points(n,ctx,X,Y,R,connexions[i][0],connexions[i][1],width_connections)
     }
-    
+
 
    // connections_points(ctx,X,Y,R*(3/4),10,10,100,100);
 
@@ -100,19 +128,7 @@ function coordY(n,i){
 
 
 
-function text_cercle(ctx,X,Y,R,tab){
-  for (i=0; i<tab.length;i++){
-    ctx.beginPath();
-    var angle1 = i*(2*Math.PI/n);
-    var angle2 = (i+1)*(2*Math.PI/n);
-    var bissectrice1 = (angle2 - angle1)/2 + angle1;
-    var X1 = X + R*Math.cos(bissectrice1);
-    var Y1 = Y + R*Math.sin(bissectrice1);
-    ctx.font = "30px Arial";
-    ctx.fillText(tab[i][1],X1,Y1);
-    
-  }
-}
+
 
 function clear_acteurs(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -155,7 +171,7 @@ var activate=0;
 
 document.getElementById('noeud1').onmouseenter=function(e){
   var connexions=[[6,2],[6,3],[6,7]];
-  
+
   clear_acteurs();
   trace_acteurs(cat_acteurs,tab_acteurs,connexions);
 };
@@ -177,5 +193,5 @@ document.getElementById('noeud1').onmouseleave=function(e){
     clear_acteurs();
     trace_acteurs(cat_acteurs,tab_acteurs,connexions);
   }
-  
+
 };
