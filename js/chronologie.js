abf2348fcb8da4b248725807c3ec783bc9b8ee96
ArthurVerrez <33450143+ArthurVerrez@users.noreply.github.com>
2018-06-02 14:09:12 +0200

    Chronologie

--- js/chronologie.js
+++ js/chronologie.js
@@ -2,19 +2,64 @@ var timelineSwiper = new Swiper(".timeline .swiper-container", {
   direction: "vertical",
   loop: false,
   speed: 1600,
-  pagination: ".swiper-pagination",
-  paginationBulletRender: function(swiper, index, className) {
+  pagination: {
+    el:".swiper-pagination",
+    type:"bullets",
+    clickable:true,
+    dynamicBullets:true,
+    dynamicMainBullets:1,
+    renderBullet: function (index, className) {
     var year = document
       .querySelectorAll(".swiper-slide")
       [index].getAttribute("data-year");
     return '<span class="' + className + '">' + year + "</span>";
+    },
   },
-  paginationClickable: true,
-  nextButton: ".swiper-button-next",
-  prevButton: ".swiper-button-prev",
+  
+  /*paginationBulletRender: function(swiper, index, className) {
+    var year = document
+      .querySelectorAll(".swiper-slide")
+      [index].getAttribute("data-year");
+    return '<span class="' + className + '">' + year + "</span>";
+  },*/
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   breakpoints: {
     768: {
       direction: "horizontal"
     }
   }
 });
+
+/*var timelineSwiper = new Swiper(".timeline .swiper-container", {
+  direction: "vertical",
+  loop: false,
+  speed: 1600,
+  pagination:{
+    el:'.swiper-pagination',
+    type:'bullets',
+    renderBullet: function(index, className) {
+      var year = document
+        .querySelectorAll(".swiper-slide")
+        [index].getAttribute("data-year");
+      return '<span class="' + className + '">' + year + "</span>";
+    },
+
+  clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+,
+  
+
+  breakpoints: {
+    768: {
+      direction: "horizontal"
+    }
+  }
+});*/
