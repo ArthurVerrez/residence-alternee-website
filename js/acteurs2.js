ea6f1fab05be50f80b8058fa9f4fe54b75873839
PaulGarnierMuller <Poloernest1997>
2018-06-11 19:43:22 +0200

    placement full texte

--- js/acteurs2.js
+++ js/acteurs2.js
@@ -46,6 +46,105 @@ function erase_acteur(id){
   object.textContent = ""
 }
 
+function place_X(i){
+  if (i === 0) {
+    return -150;
+  } else if (i === 1) {
+    return -60;
+  } else if (i === 2) {
+    return 0;
+  } else if (i === 3) {
+    return -270;
+  } else if (i === 4) {
+    return -15;
+  }
+}
+
+function place_Y(i){
+  if (i === 0) {
+    return 0;
+  } else if (i === 1) {
+    return -110;
+  } else if (i === 2) {
+    return 50;
+  } else if (i === 3) {
+    return 130;
+  } else if (i === 4) {
+    return 150;
+  }
+}
+
+function place_x(i){
+  if (i === 0) {
+    return -150;
+  } else if (i === 1) {
+    return -110;
+  } else if (i === 2) {
+    return -100;
+  } else if (i === 3) {
+    return -80;
+  } else if (i === 4) {
+    return -15;
+  } else if (i === 5) {
+    return -10;
+  } else if (i === 6) {
+    return 0;
+  } else if (i === 7) {
+    return 0;
+  } else if (i === 8) {
+    return -20;
+  } else if (i === 9) {
+    return 0;
+  } else if (i === 10) {
+    return 0;
+  } else if (i === 11) {
+    return -10;
+  } else if (i === 12) {
+    return -20;
+  } else if (i === 13) {
+    return -100;
+  } else if (i === 14) {
+    return -140;
+  } else if (i === 15) {
+    return -120;
+}
+}
+
+function place_y(i){
+  if (i === 0) {
+    return -50;
+  } else if (i === 1) {
+    return -80;
+  } else if (i === 2) {
+    return 0;
+  } else if (i === 3) {
+    return -20;
+  } else if (i === 4) {
+    return -20;
+  } else if (i === 5) {
+    return -10;
+  } else if (i === 6) {
+    return 0;
+  } else if (i === 7) {
+    return 20;
+  } else if (i === 8) {
+    return -20;
+  } else if (i === 9) {
+    return 0;
+  } else if (i === 10) {
+    return 10;
+  } else if (i === 11) {
+    return 10;
+  } else if (i === 12) {
+    return 40;
+  } else if (i === 13) {
+    return 40;
+  } else if (i === 14) {
+    return 10;
+  } else if (i === 15) {
+    return  -10;
+}
+}
 
 function circle_arcs_2points(n,ctx,X,Y,R,i,j,width_connections){
 
@@ -147,13 +246,12 @@ function trace_acteurs(cat_acteurs,tab_acteurs,connexions){
       if (cat_acteurs[i][0].length == 1) {
 
         var angle = cat_acteurs[i][0][0]*(2*Math.PI/n);
-        var X1 = X + (4*R/3)*Math.cos(angle) + add_place_text(angle)[0];
-        var Y1 = Y + (4*R/3)*Math.sin(angle) + add_place_text(angle)[1];
-        var X1 = X + (5*R/4)*Math.cos(angle);
-        var Y1 = Y + (5*R/4)*Math.sin(angle);
+        var X1 = X + (4*R/3)*Math.cos(angle) + place_X(i);
+        var Y1 = Y + (4*R/3)*Math.sin(angle) + place_Y(i);
+
 
         ctx.beginPath();
-        ctx.font = "30px Georgia";
+        ctx.font = "40px Georgia";
         ctx.fillStyle = cat_acteurs[i][2];
         ctx.fillText(cat_acteurs[i][1],X1,Y1);
         ctx.stroke();
@@ -165,13 +263,12 @@ function trace_acteurs(cat_acteurs,tab_acteurs,connexions){
         var angle1 = cat_acteurs[i][0][0]*(2*Math.PI/n);
         var angle2 = cat_acteurs[i][0][taille]*(2*Math.PI/n);
         var bissectrice1 = (angle2 - angle1)/2 + angle1;
-        var X1 = X + (4*R/3)*Math.cos(bissectrice1)+ add_place_text(bissectrice1)[0];
-        var Y1 = Y + (4*R/3)*Math.sin(bissectrice1)+ add_place_text(bissectrice1)[1];
-        var X1 = X + (5*R/4)*Math.cos(bissectrice1);
-        var Y1 = Y + (5*R/4)*Math.sin(bissectrice1);
+        var X1 = X + (4*R/3)*Math.cos(bissectrice1)+ place_X(i);
+        var Y1 = Y + (4*R/3)*Math.sin(bissectrice1)+ place_Y(i);
+
 
         ctx.beginPath();
-        ctx.font = "30px Georgia";
+        ctx.font = "40px Georgia";
         ctx.fillStyle = cat_acteurs[i][2];
         ctx.fillText(cat_acteurs[i][1],X1,Y1);
         ctx.stroke();
@@ -183,10 +280,8 @@ function trace_acteurs(cat_acteurs,tab_acteurs,connexions){
     ctx.strokeStyle="black";
     for(i=0; i<connexions.length;i++){
       /*
-      circle_arcs_center(n,ctx,X,Y,R,connexions[i][0],connexions[i][1],width_connections);
-      for(j=0; j<2;j++){
 
-        var Angle = connexions[i][j]*(2*Math.PI/n);
+        var Angle = connexions[i]*(2*Math.PI/n);
         var XA = X + (6*R_pcircle/5)*Math.cos(Angle);//+ add_place_text(angle))[0];
         var YA = Y + (6*R_pcircle/5)*Math.sin(Angle);//+ add_place_text(angle))[1];
 
@@ -195,18 +290,18 @@ function trace_acteurs(cat_acteurs,tab_acteurs,connexions){
         ctx.fillStyle = cat_acteurs[tab_acteurs[connexions[i][j]][1]][2];
         ctx.fillText(tab_acteurs[connexions[i][j]][0],XA,YA);
         ctx.stroke();
-        */
+*/
       circle_arcs_center(n,ctx,X,Y,R,connexions[i],width_connections);
 
 
-        var Angle = connexions[i][j]*(2*Math.PI/n);
-        var XA = X + (6*R_pcircle/5)*Math.cos(Angle);//+ add_place_text(angle))[0];
-        var YA = Y + (6*R_pcircle/5)*Math.sin(Angle);//+ add_place_text(angle))[1];
+        var Angle = connexions[i]*(2*Math.PI/n);
+        var XA = X + (6*R_pcircle/5)*Math.cos(Angle) + place_x(connexions[i]);//+ add_place_text(angle))[0];
+        var YA = Y + (6*R_pcircle/5)*Math.sin(Angle) + place_y(connexions[i]);//+ add_place_text(angle))[1];
 
         ctx.beginPath();
         ctx.font = "20px Georgia";
         ctx.fillStyle = cat_acteurs[tab_acteurs[connexions[i]][1]][2];
-        ctx.fillText(tab_acteurs[connexions[i]],XA,YA);
+        ctx.fillText(tab_acteurs[connexions[i]][0],XA,YA);
         ctx.stroke();
     }
 
